917dcc75dc76643ab5459d72ff90ee4ed7bf2517
suupia <suupia771@gmail.com>
2024-12-26 23:23:55 +0900

    Refactor EnemyGeneratorMono

--- Assets/MyAssets/Enemy/Scripts/EnemyGeneratorMono.cs
+++ Assets/MyAssets/Enemy/Scripts/EnemyGeneratorMono.cs
@@ -13,13 +13,13 @@ namespace Model.Enemy
         [SerializeField] Option<EnemyBabyViewMono> _enemyBabyViewMono;
 
         EnemyObjectPool _enemyObjectPool = null!;
-        
+
         [Inject]
         public void Construct(EnemyObjectPool enemyObjectPool)
         {
             _enemyObjectPool = enemyObjectPool;
         }
-
+        
         public Option<EnemyView> GenerateEnemy(EnemyKind enemyKind)
         {
             switch (enemyKind)
@@ -31,21 +31,8 @@ namespace Model.Enemy
                             Debug.LogError("No Adult Enemy prefab available.");
                             return Function.none; // Return the initial None result
                         },
-                        Some: enemyPrefab =>
-                        {
-                            var enemy = _enemyObjectPool.GetEnemy();
+                        Some: enemyPrefab => GenerateEnemyAdult(enemyPrefab));
 
-                            return enemy.Match<Option<EnemyView>>(
-                                None: () =>
-                                {
-                                    var enemyInstance = Instantiate(enemyPrefab);
-                                    return new EnemyView(enemyInstance, enemyInstance);
-                                },
-                                Some: existingEnemy =>
-                                {
-                                    return Function.Some(existingEnemy);
-                                });
-                        });
 
                 case EnemyKind.Baby:
                     return _enemyBabyViewMono.Match<Option<EnemyView>>(
@@ -54,27 +41,54 @@ namespace Model.Enemy
                             Debug.LogError("No Adult Enemy prefab available.");
                             return Function.none; // Return the initial None result
                         },
-                        Some: enemyPrefab =>
-                        {
-                            var enemy = _enemyObjectPool.GetEnemy();
-
-                            return enemy.Match<Option<EnemyView>>(
-                                None: () =>
-                                {
-                                    var enemyInstance = Instantiate(enemyPrefab);
-                                    return new EnemyView(enemyInstance, enemyInstance);
-                                },
-                                Some: existingEnemy =>
-                                {
-                                    return Function.Some(existingEnemy);
-                                });
-                        });
+                        Some: enemyPrefab => GenerateEnemyBaby(enemyPrefab));
 
                 default:
                     Debug.LogError($"Unhandled EnemyKind: {enemyKind}");
                     return Function.none;
             }
         }
-        
+
+        EnemyView InstantiateEnemyAdultView(EnemyAdultViewMono enemyAdultViewMono)
+        {
+            var enemyInstance = Instantiate(enemyAdultViewMono);
+            return new EnemyView(enemyInstance, enemyInstance);
+        }
+
+        EnemyView InstantiateEnemyBabyView(EnemyBabyViewMono enemyBabyViewMono)
+        {
+            var enemyInstance = Instantiate(enemyBabyViewMono);
+            return new EnemyView(enemyInstance, enemyInstance);
+        }
+
+
+        Option<EnemyView> GenerateEnemyAdult(EnemyAdultViewMono enemyAdultViewMono)
+        {
+            var enemy = _enemyObjectPool.GetEnemy();
+
+            return enemy.Match<Option<EnemyView>>(
+                None: () =>
+                {
+                    var enemyInstance = InstantiateEnemyAdultView(enemyAdultViewMono);
+                    return new EnemyView(enemyInstance, enemyInstance);
+                },
+                Some: existingEnemy => existingEnemy);
+        }
+
+        Option<EnemyView> GenerateEnemyBaby(EnemyBabyViewMono enemyBabyViewMono)
+        {
+            var enemy = _enemyObjectPool.GetEnemy();
+
+            return enemy.Match<Option<EnemyView>>(
+                None: () =>
+                {
+                    var enemyInstance = InstantiateEnemyBabyView(enemyBabyViewMono);
+                    return new EnemyView(enemyInstance, enemyInstance);
+                },
+                Some: existingEnemy => existingEnemy);
+        }
+
+
+
     }
 }
