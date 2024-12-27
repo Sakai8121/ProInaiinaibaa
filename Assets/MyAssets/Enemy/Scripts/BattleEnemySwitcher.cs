5011f6a6b676ffe9b80fb60fb0417486d316b2ab
suupia <suupia771@gmail.com>
2024-12-27 14:31:36 +0900

    Refactor GenerateWaitingEnemy()

--- Assets/MyAssets/Enemy/Scripts/BattleEnemySwitcher.cs
+++ Assets/MyAssets/Enemy/Scripts/BattleEnemySwitcher.cs
@@ -19,17 +19,17 @@ namespace Model.Enemy
 
         readonly EnemyGeneratorMono _enemyGeneratorMono;
         readonly EnemyObjectPool _enemyObjectPool;
-        
+
         [Inject]
-        public BattleEnemySwitcher(EnemyGeneratorMono enemyGeneratorMono,EnemyObjectPool enemyObjectPool)
+        public BattleEnemySwitcher(EnemyGeneratorMono enemyGeneratorMono, EnemyObjectPool enemyObjectPool)
         {
             _enemyGeneratorMono = enemyGeneratorMono;
             _enemyObjectPool = enemyObjectPool;
-            
+
             WaitingEnemyList = new List<Option<EnemyMono>>();
 
             _currentWaitingEnemyCount = 1;
-            GenerateWaitingEnemy(2);
+            GenerateWaitingEnemy(_enemyGeneratorMono, 2);
             SwitchToNextEnemy();
         }
 
@@ -37,14 +37,16 @@ namespace Model.Enemy
         {
             _enemyObjectPool.AddDestroyedEnemy(DefeatedEnemy);
             DefeatedEnemy = CurrentBattleEnemy;
+
             // 1. 現在の戦闘敵を切り替え
-            CurrentBattleEnemy = WaitingEnemyList.First();
-            WaitingEnemyList.RemoveAt(0);
+            CurrentBattleEnemy = WaitingEnemyList.FirstOrDefault();
+            if(WaitingEnemyList.Any()) WaitingEnemyList.RemoveAt(0);
 
             // 必要な数の敵を生成
             var generateCount = Mathf.Max(0, _currentWaitingEnemyCount - WaitingEnemyList.Count);
             Debug.LogError(generateCount);
-            GenerateWaitingEnemy(generateCount);
+            var enemies = GenerateWaitingEnemy(_enemyGeneratorMono, generateCount);
+            WaitingEnemyList.AddRange(enemies);
 
             // 超過分の敵を削除(ゾーン状態が終わった後は元の数に戻る)
             WaitingEnemyList = WaitingEnemyList.Take(_currentWaitingEnemyCount).ToList();
@@ -52,29 +54,29 @@ namespace Model.Enemy
 
         public void ChangeWaitingEnemyCount(int enemyCount)
         {
-            _currentWaitingEnemyCount = Mathf.Max(1,enemyCount);
+            _currentWaitingEnemyCount = Mathf.Max(1, enemyCount);
         }
 
-        void GenerateWaitingEnemy(int generateWaitingEnemyCount)
-        {
-            for (int i = 0; i < generateWaitingEnemyCount; i++)
-            {
-                var randomEnemyKind = DecideRandomEnemyKind();
-                var newEnemy = _enemyGeneratorMono.GenerateEnemy(randomEnemyKind);
-                newEnemy.Do(ememy =>
-                {
-                    ememy.gameObject.SetActive(true);
-                    ememy.EnemyViewMono.InitSprite();
-                });
-                WaitingEnemyList.Add(newEnemy);
-            }
-        }
+        static List<Option<EnemyMono>> GenerateWaitingEnemy(EnemyGeneratorMono enemyGeneratorMono, int generateWaitingEnemyCount)
+            => Enumerable
+                    .Range(0, generateWaitingEnemyCount)
+                    .Select(_ =>
+                    {
+                        var randomEnemyKind = DecideRandomEnemyKind();
+                        var newEnemy = enemyGeneratorMono.GenerateEnemy(randomEnemyKind);
+                        newEnemy.Do(enemy =>
+                        {
+                            enemy.gameObject.SetActive(true);
+                            enemy.EnemyViewMono.InitSprite();
+                        });
+                        return newEnemy;
+                    }).ToList();
 
-        EnemyKind DecideRandomEnemyKind()
+        static EnemyKind DecideRandomEnemyKind()
         {
             var randomProbability = Random.Range(0, 100);
             const float babyAppearProbability = 70;
-            
+
             if (randomProbability < babyAppearProbability)
                 return EnemyKind.Baby;
             else
