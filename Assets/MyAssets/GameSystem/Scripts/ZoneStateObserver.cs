a1150b6f6afab10695a059865a89af69b833111f
SakaiEiji <haku20031104@gmail.com>
2024-12-29 04:14:55 +0900

    Implement StopZoneMode

--- Assets/MyAssets/GameSystem/Scripts/ZoneStateObserver.cs
+++ Assets/MyAssets/GameSystem/Scripts/ZoneStateObserver.cs
@@ -1,11 +1,15 @@
 ﻿#nullable enable
+using System;
+using System.Threading;
 using CodeRedCat._4kVectorLandscape.Demo.Scripts;
+using Cysharp.Threading.Tasks;
 using General;
 using Model.Enemy;
 using Model.Player;
 using UniRx;
 using VContainer;
 using VContainer.Unity;
+using CancellationTokenSource = System.Threading.CancellationTokenSource;
 
 namespace MyAssets.GameSystem.Scripts
 {
@@ -15,6 +19,9 @@ namespace MyAssets.GameSystem.Scripts
         BattleEnemySwitcher _battleEnemySwitcher;
         BackGroundViewMono _backGroundViewMono;
         CameraScroll _cameraScroll;
+        ZoneStateHolder _zoneStateHolder;
+        CancellationTokenSource _cancellationTokenSource;
+        DisposeManager _disposeManager;
         
         [Inject]
         public ZoneStateObserver(ZoneStateHolder zoneStateHolder,PlayerStateHolder playerStateHolder,
@@ -25,6 +32,10 @@ namespace MyAssets.GameSystem.Scripts
             _battleEnemySwitcher = battleEnemySwitcher;
             _backGroundViewMono = backGroundViewMono;
             _cameraScroll = cameraScroll;
+            _zoneStateHolder = zoneStateHolder;
+            _disposeManager = disposeManager;
+            
+            _cancellationTokenSource = new CancellationTokenSource();
             
             zoneStateHolder.ObserveEveryValueChanged(holder => holder.CurrentZoneState)
                 .Subscribe(currentZoneState =>
@@ -51,14 +62,35 @@ namespace MyAssets.GameSystem.Scripts
             _battleEnemySwitcher.ChangeWaitingEnemyCount(3);
             _backGroundViewMono.ChangeToZoneBackGround();
             _cameraScroll.StopCameraMove();
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            _disposeManager.CompositeDisposable.Add(_cancellationTokenSource);
+            WaitInZoneTimeUniTask(_cancellationTokenSource);
         }
 
         void ExecuteActionOnNotZone()
         {
+            _cancellationTokenSource.Cancel();
             _playerStateHolder.ChangePlayerState(PlayerStateHolder.PlayerState.Default);
             _battleEnemySwitcher.ChangeWaitingEnemyCount(1);
             _backGroundViewMono.RevertCurrentBackGround();
             _cameraScroll.ReStartCameraMove();
+        }
+
+        async void WaitInZoneTimeUniTask(CancellationTokenSource cancellationTokenSource)
+        {
+            try
+            {
+                // 5秒待機中にキャンセル可能
+                await UniTask.Delay(TimeSpan.FromSeconds(5.0f), cancellationToken: cancellationTokenSource.Token);
+
+                // キャンセルされなかった場合の処理
+                _zoneStateHolder.EndZoneMode();
+            }
+            catch (OperationCanceledException)
+            {
+                // キャンセル時の処理
+            }
         }
     }
 }
